a67ca0807cb958c7c363a6a7c4ed944e688104a2
sony-jun <warrise@naver.com>
2022-09-30 14:25:40 +0900

    장고뼈대만듦

--- re_pair/views.py
+++ re_pair/views.py
@@ -10,8 +10,12 @@ def index(request):
     return render(request, "re_pair/index.html", context)
 
 
-def detail(request):
-    return render(request, "re_pair/detail.html")
+def detail(request, pk_):
+    review = Review.objects.get(pk=pk_)
+    context = {
+        "review": review,
+    }
+    return render(request, "re_pair/detail.html", context)
 
 
 def new(request):
@@ -28,4 +32,32 @@ def create(request):
         "content": content,
     }
 
+    return redirect("re_pair:index")
+
+
+def edit(request, pk_):
+    # get 메소드를 사용해서 특정 pk 데이터를 불러온다.
+    review = Review.objects.get(pk=pk_)
+    context = {
+        "review": review,
+    }
+    return render(request, "re_pair/edit.html", context)
+
+
+def update(request, pk_):
+    review = Review.objects.get(pk=pk_)
+
+    title_ = request.GET.get("title")
+    content_ = request.GET.get("content")
+
+    review.title = title_
+    review.content = content_
+
+    review.save()
+
+    return redirect("re_pair:detail", review.pk)
+
+
+def delete(request, pk):
+    Review.objects.get(id=pk).delete()
     return redirect("re_pair:index")
